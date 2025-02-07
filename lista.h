e27ced83bd0896cf999632c2e1de6209bb7899a1
Alvaro Tardin <166247858+alvarotig@users.noreply.github.com>
2025-02-07 19:48:15 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,27 +1,24 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-typedef struct Passageiro 
-{
-    char nome[101];
-    char RG[101];
-    char Assento[3]; 
-    char Destino[101];
-} Passageiro;
+struct passageiro{
+  char nome[101];
+  char RG[101];
+  char Assento [3];
+  char Destino [101];
+};
 
-typedef struct 
-{
-    Passageiro passageiro;
-    Passageiro *anterior;
-    Passageiro *posterior;
-} EleList;
+typedef struct lista lista;
+struct lista{
+    passageiro *prox, *prev;
+    passageiro passageiro;
+};
 
-int criar (); 
-int destruir (); 
-int addinicio (); 
-int addfim (); 
-int reminicio (); 
-int remfim (); 
-int vazio ();  
-
+int CriarLista (lista** lista); 
+int DestruirLista (lista** lista); 
+int AddInicioLista (lista* lista, passageiro passageiro); 
+int AddFimLista (lista* lista, passageiro passageiro); 
+int RemInicioLista (lista* lista); 
+int RemFimLista (lista* lista); 
+int VazioLista (lista* lista);  
 #endif
